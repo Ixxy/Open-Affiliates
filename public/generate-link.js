46ced06a482b5f2034d532f0ededf15c7067123b
Tom Kadwill <tomkadwill@gmail.com>
2012-03-20 23:09:59 +0000

    added old AFA files

---
+++ public/generate-link.js
@@ -0,0 +1,20 @@
+function graphCachePopulated() {
+    $("#com30").attr("src", "graph.php?variable=commission&days=30");
+    $("#com90").attr("src", "graph.php?variable=commission&days=90");
+    $("#ord7").attr("src", "graph.php?variable=orders&days=7");
+    $("#ord30").attr("src", "graph.php?variable=orders&days=30");
+    $("#ord90").attr("src", "graph.php?variable=orders&days=90");
+}
+
+$(function() {
+    $("#tabs > ul").tabs();
+    $("#com7").attr("src", "graph.php?variable=commission&days=7&fromcache=0");
+    $("#com7").load(graphCachePopulated);
+});
+
+function generateUrl() {
+	url = inputURL.value;
+	url += '?ref=1';
+	document.getElementById('results').innerHTML=url;
+	document.getElementById('results').target="_blank";
+}
